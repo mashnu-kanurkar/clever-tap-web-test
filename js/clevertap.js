43a5bf33e9186d58de3f0a2101d13fdbad218d02
mashnu-kanurkar <87574265+mashnu-kanurkar@users.noreply.github.com>
2024-06-05 17:18:10 +0530

    Update clevertap.js

--- js/clevertap.js
+++ js/clevertap.js
@@ -1,7 +1,6 @@
-export class clevertap{
-  function sendEventToCT(e, o) {
-  // event with properties
-  console.log("event", e)
-  console.log("props", o)
-clevertap.event.push(e, o);
-}}
+var customCleverTap = {
+    sendPayload: function(payload) {
+        console.log("Sending payload:", payload);
+        // Your custom implementation
+    }
+};
