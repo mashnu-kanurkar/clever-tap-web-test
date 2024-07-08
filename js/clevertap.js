c22cce1c8a41531ee221f61395763b2c9096daf2
mashnu-kanurkar <87574265+mashnu-kanurkar@users.noreply.github.com>
2024-07-08 19:36:25 +0530

    Update clevertap.js

--- js/clevertap.js
+++ js/clevertap.js
@@ -1,8 +1,1 @@
-export let clevertap =  Clevertap{
-    constructor(){}
-    sendEventToCT(e,o){
-    console.log("event", e)
-        console.log("props", o)
-            clevertap.event.push(e, o);
-    }
-}
+
